a0ded4295348f833b021efb03bd77a189c2c2307
kundan0000007 <kkumar00346@gmail.com>
2017-08-07 16:24:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Article-one',function(req,res)
+{
+    res.send('Article one is requested and send here');
+});
+
+app.get('/Article-two',function(req,res)
+{
+    res.send('Article two is requested and send here');
+});
+
+app.get('/Article-three',function(req,res)
+{
+    res.send('Article three is requested and send here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
