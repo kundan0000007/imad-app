3172c8788819b7044a582d841089806d91405d38
kundan0000007 <kkumar00346@gmail.com>
2017-08-08 03:13:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
 var img = document.getElementBydId('madi') ;
-img.onClick = function(){
-    img.style.marginLeft='100px';
+
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft = 10;
+    img.style.marginLeft = marginLeft = 'px';
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight, 100);
 }
