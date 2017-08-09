6001e337648f1d651cc426cb99ca213aab810c78
kundan0000007 <kkumar00346@gmail.com>
2017-08-09 19:59:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,12 @@ function moveRight(){
     marginLeft = marginLeft = 10;
     img.style.marginLeft = marginLeft = 'px';
 }
-img.onClick = function(){
-    var interval = setInterval(moveRight, 100);
-};
+window.onload = function()
+    {
+        img.onClick = function(){
+        var interval = setInterval(moveRight, 100);
+    };
+    };
 
 //counter code
 var button = document.getElementById('counter');
