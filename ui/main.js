dec625cc0038ed415a138d41ff304fe8a83e9583
kundan0000007 <kkumar00346@gmail.com>
2017-08-09 16:55:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,3 +10,29 @@ img.onclick = function(){
     var interval = setInterval(moveRight, 100);
 };
 
+//counter code
+var button = document.getElementById('counter');
+
+buttom.onClick = function(){
+    //create a request object 
+    var request = new XMLHttpRequest();
+    
+    //caoture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.get.ElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
+    };
+    
+    //make the requesft
+    reques.open('GET',"http://kkumar00346.imad.hasura-app.io/",true);
+    request.sent(null);
+    
+    
+}
