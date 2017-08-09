bfa4511a8447fdac54a22edd30131f821db166ff
kundan0000007 <kkumar00346@gmail.com>
2017-08-09 17:53:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,11 +15,20 @@ img.onclick = function(){
 
 
 
-var button = document.getElementById('counter');
+
 var counter = 0;
 
 button.onClick = function () {
     counter=counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
+
+            var button = document.getElementById('counter');
+var counter = 0;
+
+button.onClick = function () {
+    counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
