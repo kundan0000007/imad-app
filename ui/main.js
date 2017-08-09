5899cc01dca001235021d29a144279812990c8b6
kundan0000007 <kkumar00346@gmail.com>
2017-08-09 17:56:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,22 +11,26 @@ img.onclick = function(){
 };
 
 //counter code
+var button = document.getElementById('counter');
 
-
-
- 
-
-button.onClick = function () {
-    counter=counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-};
-
-            var button = document.getElementById('counter');
-var counter = 0;
-
-button.onClick = function () {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+button.onClick = function() {
+    //create a request object 
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
+    };
+    
+    //make the requesft
+    request.open('GET','http://kkumar00346.imad.hasura-app.io/',true);
+    request.send(null);
 };
