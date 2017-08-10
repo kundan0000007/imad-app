c8dd17750cf95bbf3bc6ca2e5674830d4030cec3
kundan0000007 <kkumar00346@gmail.com>
2017-08-10 12:17:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,3 +40,26 @@ button.onClick = function() {
 };
 }
 ;
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit.btn');
+submit.onclick = function () {
+    //make a request to the server and send the name
+    
+    //capture a list of name and render it as a list
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for (var i=0; i<names.length;i++)
+    {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+    
+};
+
+
+
+
